d43cf5e5eafdcf11e1787420609d6cced789522d
mydew <mydew1992@gmail.com>
2020-12-20 18:08:05 +0100

    quick temp static files path modification for a test

--- docs/precache-manifest.5271a9ab2d8bc9f2adedba115d5c1ba5.js
+++ docs/precache-manifest.5271a9ab2d8bc9f2adedba115d5c1ba5.js
@@ -5,26 +5,26 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   },
   {
     "revision": "6ae5b7a13264cd5d19aa",
-    "url": "/static/css/2.7ac39a5d.chunk.css"
+    "url": "/expenses/static/css/2.7ac39a5d.chunk.css"
   },
   {
     "revision": "91ca8e5bd887eb7b8a29",
-    "url": "/static/css/main.4d4eb2ea.chunk.css"
+    "url": "/expenses/static/css/main.4d4eb2ea.chunk.css"
   },
   {
     "revision": "6ae5b7a13264cd5d19aa",
-    "url": "/static/js/2.fa8636d4.chunk.js"
+    "url": "/expenses/static/js/2.fa8636d4.chunk.js"
   },
   {
     "revision": "f14083e6e3fa69b0fba330ce12367a6d",
-    "url": "/static/js/2.fa8636d4.chunk.js.LICENSE.txt"
+    "url": "/expenses/static/js/2.fa8636d4.chunk.js.LICENSE.txt"
   },
   {
     "revision": "91ca8e5bd887eb7b8a29",
-    "url": "/static/js/main.23675639.chunk.js"
+    "url": "/expenses/static/js/main.23675639.chunk.js"
   },
   {
     "revision": "f31b34e9a7a60b9e51c0",
-    "url": "/static/js/runtime-main.e901c6d6.js"
+    "url": "/expenses/static/js/runtime-main.e901c6d6.js"
   }
 ]);
