40e06bdd87cb09001cbb3f114ee46cc3a2d3ce9d
mydew <mydew1992@gmail.com>
2020-12-21 22:01:39 +0100

    new docs generated

--- deploy.sh
+++ deploy.sh
@@ -14,8 +14,8 @@ REACT_APP_SHEET_ID=1DEBrE6KXBxdAaWnWfS-e0u2aFawrephhzmH81Ne5sxw npm run build
 
 # github pages
 npm run build
-cp -a build/. production/
-cd production
+cp -a build/. docs/
+cd docs
 git add .
 git commit -m "$1"
 git push origin feature/github-pages
