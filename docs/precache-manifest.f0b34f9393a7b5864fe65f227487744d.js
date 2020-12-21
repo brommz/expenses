9125311cec6bddaf3871d2bbb92cccb7608d4696
mydew <mydew1992@gmail.com>
2020-12-21 22:14:42 +0100

    add homepage to package.json

---
+++ docs/precache-manifest.f0b34f9393a7b5864fe65f227487744d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bd9d004381554036b32247f74a942ef",
+    "url": "/expenses/index.html"
+  },
+  {
+    "revision": "7681eb33479fff176d53",
+    "url": "/expenses/static/css/2.7ac39a5d.chunk.css"
+  },
+  {
+    "revision": "fa1810dea82253454cd4",
+    "url": "/expenses/static/css/main.4d4eb2ea.chunk.css"
+  },
+  {
+    "revision": "7681eb33479fff176d53",
+    "url": "/expenses/static/js/2.7972a520.chunk.js"
+  },
+  {
+    "revision": "f14083e6e3fa69b0fba330ce12367a6d",
+    "url": "/expenses/static/js/2.7972a520.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa1810dea82253454cd4",
+    "url": "/expenses/static/js/main.475c536a.chunk.js"
+  },
+  {
+    "revision": "9af731b91e08f6d9bb9c",
+    "url": "/expenses/static/js/runtime-main.ad3aa597.js"
+  }
+]);
