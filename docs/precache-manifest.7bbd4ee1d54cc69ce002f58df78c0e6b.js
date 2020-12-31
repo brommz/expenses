1ce9e5b83e4754cd7e6acb39f9e842cc041d0ba3
brommz <mydew1992@gmail.com>
2020-12-31 23:40:55 +0100

    feat - custom filtering exp cat

---
+++ docs/precache-manifest.7bbd4ee1d54cc69ce002f58df78c0e6b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47f2efc70fd1a9483b160700be3c89fe",
+    "url": "https://brommz.github.io/expenses/index.html"
+  },
+  {
+    "revision": "7034e8ed8f590ca3cdff",
+    "url": "https://brommz.github.io/expenses/static/css/2.38030236.chunk.css"
+  },
+  {
+    "revision": "8deed7bf93ae90573ba5",
+    "url": "https://brommz.github.io/expenses/static/css/main.4d4eb2ea.chunk.css"
+  },
+  {
+    "revision": "7034e8ed8f590ca3cdff",
+    "url": "https://brommz.github.io/expenses/static/js/2.fa3c59af.chunk.js"
+  },
+  {
+    "revision": "4ed2c42aa092cb6cb66a12b40c522332",
+    "url": "https://brommz.github.io/expenses/static/js/2.fa3c59af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8deed7bf93ae90573ba5",
+    "url": "https://brommz.github.io/expenses/static/js/main.c3c7ccd4.chunk.js"
+  },
+  {
+    "revision": "d13ddaee609e172cdc12",
+    "url": "https://brommz.github.io/expenses/static/js/runtime-main.07baa3c1.js"
+  }
+]);
