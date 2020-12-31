f46862654865ae4c93e0f5df5ad68af6cc1a0640
brommz <mydew1992@gmail.com>
2020-12-31 20:22:00 +0100

    deploy - 28.12.2020 v7

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 export REACT_APP_SHEET_ID=1Z-1RXh51c74cw4KMzuSfZ_8NZ34qbNz0McOZMLOyOrY 
 export REACT_CLIENT_ID=758679234250-t9fh8t8ph9pssqb5kekseinfgout8f9i 
+# export PUBLIC_URL=https://brommz.github.io/expenses
 echo $REACT_APP_SHEET_ID
 echo $REACT_CLIENT_ID
 
